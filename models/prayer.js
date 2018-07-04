1ee3667bedbc131a73a9e1e58403165f71230f5a
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-04 16:42:53 +0700

    added validation to models

--- models/prayer.js
+++ models/prayer.js
@@ -1,7 +1,33 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+  const Op = sequelize.Op;
   var prayer = sequelize.define('prayer', {
-    prayer_name: DataTypes.STRING
+    prayer_name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true
+        isUnique: function(value, callback) {
+          prayer.findOne({
+            where: {
+              prayer_name: value,
+              id: {
+                [Op.ne]: this.id
+              }
+            },
+          })
+          .then((result) => {
+            if (result !== null) {
+              callback("A prayer with the entered name has already been registered");
+            } else {
+              callback();
+            }
+          })
+          .catch((err) => {
+            callback(err);
+          })
+        }
+      },
+    }
   }, {});
   prayer.associate = function(models) {
     // associations can be defined here
--- models/user.js
+++ models/user.js
@@ -1,13 +1,77 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+  const Op = sequelize.Op;
   var user = sequelize.define('user', {
-    full_name: DataTypes.STRING,
-    user_type: DataTypes.STRING,
-    password: DataTypes.INTEGER,
+    full_name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        is: ["^[a-z]+$", 'i'],
+        isUnique: function(value, callback) {
+          user.findOne({
+            where: {
+              full_name: value,
+              id: {
+                [Op.ne]: this.id
+              },
+            },
+          })
+          .then((result) => {
+            if (result !== null) {
+              callback("A user has already registered with the entered full name")
+            } else {
+              callback()
+            }
+          })
+          .catch((err) => {
+            callback(err)
+          })
+        }
+      },
+    },
+    user_type: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        len: [8, 30],
+      },
+    },
     salt: DataTypes.STRING,
     createdAt: new Date(),
     updatedAt: new Date(),
-    email: {type: DataTypes.STRING},
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+        is: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        isUnique: function(value, callback) {
+          user.findOne({
+              where: {
+                email: value,
+                id: {
+                  [Op.ne]: this.id
+                },
+              },
+            }).then((result) => {
+              if (result !== null) {
+                callback("A user has already registered with the entered email address")
+              } else {
+                callback()
+              }
+            })
+            .catch((err) => {
+              callback(err)
+            })
+        },
+      },
+    },
   }, {});
   user.associate = function(models) {
     // associations can be defined here
