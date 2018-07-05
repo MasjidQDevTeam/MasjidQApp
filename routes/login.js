0f890f32982862797492f50f900e2a902d26c74d
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-05 15:08:35 +0700

    init commit

--- routes/login.js
+++ routes/login.js
@@ -2,24 +2,31 @@ const login = require('express').Router();
 const models = require('../models');
 const ejs = require('ejs');
 const bcrypt = require('bcrypt');
+const saltRounds = 8;
 
 
 login.get("/", (req, res) => {
   res.render("login");
 })
 
-// login.post("/", (req, res, next) => {
-//   let input = req.body;
-//   models.user.findOne({
-//       where: {
-//         email: input.email
-//       },
-//       attributes: ["password"]
-//   )}
-//
-//
-// }, (req, res) => {
-//   res.send("success");
-// })
+login.post("/", (req, res, next) => {
+  let input = req.body;
+  models.user.findOne({
+      where: {
+        email: input.email
+      },
+      attributes: ["password"]
+    })
+    .then((result) => {
+      let passCheck = bcrypt.compareSync(input.password, result.password)
+      if (passCheck === true) {
+        next()
+      } else {
+        res.send("wrong password/email")
+      }
+    })
+}, (req, res) => {
+  res.send("success");
+})
 
 module.exports = login;
